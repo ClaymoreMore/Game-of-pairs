ebf6153ab8d62ee1d376fa37f0009fba5465adf1
dmitrii <konobeeff.dima@yandex.ru>
2024-04-11 20:10:39 +0300

    Доработал форму, таймер, кнопку "новая игра", выбор создания пар от 4 до 10 (только четные)

--- GameOfPairs/components/Container/Container.js
+++ GameOfPairs/components/Container/Container.js
@@ -1,6 +1,7 @@
 export function Container() {
   const container = document.createElement('div')
   container.className = 'container'
+  container.id = 'container'
   
   return container
   
--- GameOfPairs/main.js
+++ GameOfPairs/main.js
@@ -30,14 +30,43 @@ import { Card, showNumber, hideNumber } from './components/Card/Card.js';
   **/
 
 
+// Browser API
+// submit
+// event submit form
+// function eventFn () {}
+// eventFn({target: .... })
+
+//....
+// event => eventFn({  ....  })
 
 const form = document.getElementById('form')
+let gameTimer;
+let clearCardTimer;
+
 form.addEventListener('submit', (event) => {
+  const prevContainer = document.getElementById('container')
+
   event.preventDefault()
-  const numCards = document.getElementById('numCards')
-  const numberCard = numCards.value 
-  
+
+  const formData = new FormData(event.target);
+  const numberCard = formData.get("numCards")
+
   const numberArr = getNumArray(numberCard)
+
+  console.log(`/GameOfPairs/main.js - line: 54 ->> prevContainer`, prevContainer)
+
+  if (prevContainer) {
+    prevContainer.remove()
+  }
+
+  if (gameTimer) {
+    clearTimeout(gameTimer)
+  }
+
+  if (clearCardTimer) {
+    clearTimeout(clearCardTimer)
+  }
+
   Game(numberArr)
 })
 
@@ -49,7 +78,7 @@ function Game(cardArray) {
   let prevCard = null;
   let cardsList = [];
   let clickedButtons = [];
-
+  const winCards = [];
 
   cardArray.forEach((number) => {
     const newCard = createCard(number);
@@ -57,13 +86,13 @@ function Game(cardArray) {
     container.append(newCard)
     cardsList.push(newCard)
 
-//TODO 
-/**
-  * вынести хэндлер слушателся в отдельный файл
-  * 
-  * т/к он отвечает только за логику игры
-  * 
-  **/
+    //TODO 
+    /**
+      * вынести хэндлер слушателся в отдельный файл
+      * 
+      * т/к он отвечает только за логику игры
+      * 
+      **/
 
     newCard.addEventListener('click', function () {
       if (clickedButtons.length > 2) {
@@ -74,43 +103,79 @@ function Game(cardArray) {
       const spanCard = this.firstChild
 
       showNumber(currentCard)
-      
+
       if (prevCard === null) {
         prevCard = currentCard
         clickedButtons.push(currentCard)
+
       } else {
         clickedButtons.push(currentCard)
+
         if (prevCard === currentCard) {
           spanCard.classList.add('hidden')
           spanCard.classList.toggle('openNumberCard')
           prevCard = null
-        } else {
-          if (prevCard.firstChild.id === spanCard.id) {
-          prevCard.classList.add('winCard')
-          currentCard.classList.add('winCard')
-          prevCard.classList.remove('card')
-          currentCard.classList.remove('card')
-          prevCard.disabled = true
-          currentCard.disabled = true
-          prevCard = null
-        }
-        else {
-          spanCard.classList.toggle('hidden')
-          prevCard.firstChild.classList.toggle('hidden')
-          let cards = document.getElementsByClassName('card') // [but,but,but...] -> [0],[1].. -> disabled
-          for (let card of cards) {
-            card.disabled = true
-          }                             // в данном цикле происходит перебор buttons и отключение интерфейса у каждой кнопки по порядку
 
-          setTimeout(() => {
-            spanCard.classList.toggle('openNumberCard')
-            prevCard.firstChild.classList.toggle('openNumberCard')
-            for (let card of cards) {
-              card.disabled = false
+        } else {
+
+          if (prevCard.firstChild.id === spanCard.id) {
+            winCards.push(spanCard.id)
+            prevCard.classList.add('winCard')
+            currentCard.classList.add('winCard')
+            prevCard.classList.remove('card')
+            currentCard.classList.remove('card')
+            
+            prevCard.disabled = true
+            currentCard.disabled = true
+            
+            //TODO
+            /**
+              * if wincards.length * 2 === cardArray.length
+              * 
+              * 
+              * promt('you win') ok
+              * cancel / ok
+              * 
+              * ok => delete prev ''
+              * 
+              * cancel  => null
+              * 
+              * 
+              **/
+
+            if (winCards.length * 2 === cardArray.length) {
+              const endGameConfirm = confirm('Вы победили. Начать новую игру?')
+
+              console.log(`/GameOfPairs/main.js - line: 131 ->> endGamePrompt`, endGameConfirm)
+
+              if (endGameConfirm) {
+                container.remove()
+                
+              }
             }
+
             prevCard = null
-          }, 1_000)
-        }}
+          }
+          else {
+            spanCard.classList.toggle('hidden')
+            prevCard.firstChild.classList.toggle('hidden')
+            let cards = document.getElementsByClassName('card') // [but,but,but...] -> [0],[1].. -> disabled
+            for (let card of cards) {
+              card.disabled = true
+            }                             // в данном цикле происходит перебор buttons и отключение интерфейса у каждой кнопки по порядку
+
+            setTimeout(() => {
+              spanCard.classList.toggle('openNumberCard')
+              prevCard.firstChild.classList.toggle('openNumberCard')
+              
+              for (let card of cards) {
+                card.disabled = false
+              }
+              
+              prevCard = null
+            }, 1_000) // clean this !
+          }
+        }
 
         clickedButtons = [];
 
@@ -122,26 +187,26 @@ function Game(cardArray) {
 
   document.body.append(container)
 
-  setTimeout(() => {
+  clearCardTimer = setTimeout(() => {
     cardsList.forEach((card) => {
       hideNumber(card)
     })
-  }, 5000)
+  }, 5_000)
 
-  setTimeout(() => {
+  gameTimer = setTimeout(() => {
     let cards = document.getElementsByClassName('card')
     for (let card of cards) {
       card.disabled = true
     }
     alert('Игра окончена')
-  }, 100000)
+  }, 120_000)
 }
 
 
 //Создать кнопку "Начать новую игру" - start game(shuffleArr)
 
 // 1. Клик по кнопке
-//2.  Делаем проверку, есть ли в переменной выбранная кнопка: 
+//2.  Делаем проверку, есть ли в переменной выбранная кнопка:
 //        ур1 если нет - сохраняем выбранную кнопку и красим в серый цвет +
 //3.      ур1 Если кнопка есть - необходим ряд проверок далее:
 //            ур2 сначала необходимо сравнить выбранную кнопку с текущей кнопкой:
